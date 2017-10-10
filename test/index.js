8213769df576d576379a7e317f3a33ff59082386
code-mattclaffey <matt.claffey@codecomputerlove.com>
2017-10-10 12:08:02 +0100

    fix: borken tests.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-//require('localhost')('./components').listen(8080);
+require('localhost')('./components').listen(8080);
 
 const tabs = require('./tabs/index')();
 const accordion = require('./accordion/index')();
