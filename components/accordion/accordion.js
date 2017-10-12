2fb91dd873c7db6e8ab6cc5ab25ff1880f257b98
code-mattclaffey <matt.claffey@codecomputerlove.com>
2017-10-12 15:36:05 +0100

    feat: updated the accordion and added more tests around it.

--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -5,73 +5,45 @@
 	window.Code = window.Code || {};
 
 	window.Code.Accordion = function(component) {
-		const buttons = Array.prototype.slice.call(component.querySelectorAll('[data-accordion-button]'));
+		const accordionButtons = Array.prototype.slice.call(component.querySelectorAll('[data-accordion-button]'));
+		const accordionTabs = Array.prototype.slice.call(component.querySelectorAll('[data-accordion-content-area]'));
 
-		const ACTIVE_BUTTON = 'accordion__button--active';
-		const ACTIVE_SECTION = 'accordion__content--open';
+		const ariaExpanded = 'aria-expanded';
+		const ariaHidden = 'aria-hidden';
 
-		let activeButton = component.querySelector(`.${ACTIVE_BUTTON}`);
-		let activeSection = component.querySelector(`.${ACTIVE_SECTION}`);
-
-		function getOffsetHeight(el) {
-			const style = getComputedStyle(el);
-			return el.offsetHeight + parseInt(style.marginTop) + parseInt(style.marginBottom);
+		function showAccordionTab(button, section) {
+			button.setAttribute(ariaExpanded, true);
+			section.setAttribute(ariaHidden, false);
 		}
 
-		function animateOpenHeight(section) {
-			const height = getOffsetHeight(section.querySelector('[data-accordion-content-wrapper]'));
-			section.setAttribute(`style`, `height: ${height}px;`);
+		function hideAccordionTab(button, section) {
+			button.setAttribute(ariaExpanded, false);
+			section.setAttribute(ariaHidden, true);
 		}
 
-		function delayCloseClass(section) {
+		function closeAllAccordions() {
+			accordionButtons.forEach((element, i) => {
+				hideAccordionTab(element, accordionTabs[i]);
+			});
 		}
 
-		function closeSection(section) {
-			section.setAttribute(`style`, `height: 0px;`);
-			section.classList.remove(ACTIVE_SECTION);
-		}
+		function toggleAccordion(event) {
+			const activeButton = event.currentTarget;
+			const activeContentArea = activeButton.parentNode.querySelector('[data-accordion-content-area]');
 
-		function removeActiveButtonState(button) {
-			button.classList.remove(ACTIVE_BUTTON);
-			button.setAttribute('aria-expanded', false);
-		}
-
-		function openSection(section) {
-			section.classList.add(ACTIVE_SECTION);
-			animateOpenHeight(section);
-			activeSection = section;
-		}
-
-		function addActiveButtonState(button) {
-			button.classList.add(ACTIVE_BUTTON);
-			button.setAttribute('aria-expanded', true);
-			activeButton = button;
-		}
-
-		function closeAccordion(button, section) {
-			removeActiveButtonState(button);
-			closeSection(section);
-		}
-
-		function openAccordion(button, section) {
-
-			if(activeButton !== null) {
-				closeAccordion(activeButton, activeSection);
+			if(activeButton.getAttribute(ariaExpanded) === 'true') {
+				hideAccordionTab(activeButton, activeContentArea);
+			} else {
+				closeAllAccordions();
+				showAccordionTab(activeButton, activeContentArea);
 			}
-
-			addActiveButtonState(button);
-			openSection(section);
 		}
 
-		function handleAccordionToggle(event) {
-			const currentButton = event.currentTarget;
-			const currentSection = currentButton.parentNode.querySelector('[data-accordion-section]');
-			const isOpen = currentSection.classList.contains(ACTIVE_SECTION);
-
-			isOpen ? closeAccordion(currentButton, currentSection) : openAccordion(currentButton, currentSection);
+		function init() {
+			accordionButtons.forEach(element => element.addEventListener('click', toggleAccordion));
 		}
 
-		buttons.forEach(button => button.addEventListener('click', handleAccordionToggle));
+		init();
 	};
 
 	new window.Code.Accordion(document.querySelector('[data-accordion]'));
--- test/accordion/index.js
+++ test/accordion/index.js
@@ -10,12 +10,13 @@ const HTML = fs.readFileSync(path.resolve('components', 'accordion', 'index.html
 let dom;
 
 module.exports = function() {
-	describe('The Accordion Component ', () => {
+
+	describe('Accordion', () => {
 
 		beforeEach(function(done) {
 			dom = new JSDOM(HTML, {
-				runScripts: "dangerously",
-				resources: "usable"
+				runScripts: 'dangerously',
+				resources: 'usable'
 			});
 
 			dom.window.addEventListener('load', function() {
@@ -23,27 +24,162 @@ module.exports = function() {
 			});
 		});
 
-		it('should have aria-expanded set to true when selected', () => {
-			const Click = new dom.window.MouseEvent('click', {});
-			const buttons = dom.window.document.querySelectorAll('[data-accordion-button]');
+		it('should have an aria-label on the on the accordion to display its context', () => {
+			var truthy = false;
+			const accordion = dom.window.document.querySelector('[data-accordion]');
 
-			buttons[0].addEventListener('click', () => {
-				buttons[0].getAttribute('aria-expanded').should.equal('true');
-			});
+			if(accordion.getAttribute('aria-label') !== null) {
+				truthy = true
+			}
 
-			buttons[0].dispatchEvent(Click);
+			truthy.should.equal(true);
+
 		});
 
-		it('should have a active class on the button', () => {
-			const Click = new dom.window.MouseEvent('click', { });
-			const buttons = dom.window.document.querySelectorAll('[data-accordion-button]');
+		it('should have buttons that have an aria-controls that matches the id of the content area', () => {
+			const accordion = dom.window.document.querySelector('[data-accordion]');
+			const buttons = Array.prototype.slice.call(accordion.querySelectorAll('[data-accordion-button]'));
+			const contentArea = Array.prototype.slice.call(accordion.querySelectorAll('[data-accordion-content-area]'));
 
-			buttons[0].addEventListener('click', () => {
-				buttons[0].classList.contains('accordion__button--active').should.equal(true);
+			let expectedResult = buttons.length;
+			let totalMatching = 0;
+
+			buttons.forEach((element, i) => {
+
+				const attribute = element.getAttribute('aria-controls');
+				const contentAreaId = contentArea[i].getAttribute('id');
+
+				if(attribute !== null) {
+					if(attribute === contentAreaId) {
+						totalMatching++;
+					}
+				}
 			});
 
-			buttons[0].dispatchEvent(Click);
+			totalMatching.should.equal(expectedResult);
+		});
 
+		it('should have aria-expanded on the accordion buttons', () => {
+			const accordion = dom.window.document.querySelector('[data-accordion]');
+			const buttons = Array.prototype.slice.call(accordion.querySelectorAll('[data-accordion-button]'));
+
+			let expectedResult = buttons.length;
+			let totalMatching = 0;
+
+			buttons.forEach(element => {
+
+				const attribute = element.getAttribute('aria-expanded');
+
+				if(attribute !== null) {
+					totalMatching++;
+				}
+			});
+
+			totalMatching.should.equal(expectedResult);
+		});
+
+		it('should have aria-hidden on the accordion content areas', () => {
+			const accordion = dom.window.document.querySelector('[data-accordion]');
+			const contentArea = Array.prototype.slice.call(accordion.querySelectorAll('[data-accordion-content-area]'));
+
+			let expectedResult = contentArea.length;
+			let totalMatching = 0;
+
+			contentArea.forEach(element => {
+
+				const attribute = element.getAttribute('aria-hidden');
+
+				if(attribute !== null) {
+					totalMatching++;
+				}
+			});
+
+			totalMatching.should.equal(expectedResult);
+		});
+
+		it('should set the button aria-expanded="true" when selected', () => {
+			var truthy = false;
+			const Click = new dom.window.MouseEvent('click', {});
+
+			const accordion = dom.window.document.querySelector('[data-accordion]');
+			const button = accordion.querySelector('[data-accordion-button]');
+
+			button.addEventListener('click', (event) => {
+				if(event.currentTarget.getAttribute('aria-expanded') === 'true') {
+					truthy = true;
+				}
+			});
+
+			button.dispatchEvent(Click);
+
+			truthy.should.equal(true);
+		});
+
+		it('should set the button aria-hidden="false" when the button is selected', () => {
+			var truthy = false;
+			const Click = new dom.window.MouseEvent('click', {});
+
+			const accordion = dom.window.document.querySelector('[data-accordion]');
+			const button = accordion.querySelector('[data-accordion-button]');
+			const section = accordion.querySelector('[data-accordion-content-area]');
+
+			button.addEventListener('click', (event) => {
+				if(section.getAttribute('aria-hidden') === 'false') {
+					truthy = true;
+				}
+			});
+
+			button.dispatchEvent(Click);
+
+			truthy.should.equal(true);
+		});
+
+		it('should set the button aria-expanded="false" when selected a second time', () => {
+			var truthy = false;
+			const Click = new dom.window.MouseEvent('click', {});
+
+			const accordion = dom.window.document.querySelector('[data-accordion]');
+
+			const button = accordion.querySelector('[data-accordion-button]');
+			const contentArea = accordion.querySelector('[data-accordion-content-area]');
+
+			// set the first one to be active
+			button.setAttribute('aria-expanded', true);
+			contentArea.setAttribute('aria-hidden', false);
+
+			button.addEventListener('click', (event) => {
+				if(event.currentTarget.getAttribute('aria-expanded') !== 'true') {
+					truthy = true;
+				}
+			});
+
+			button.dispatchEvent(Click);
+
+			truthy.should.equal(true);
+		});
+
+		it('should set the button aria-hidden="true" when the button is selected a second time', () => {
+			var truthy = false;
+			const Click = new dom.window.MouseEvent('click', {});
+
+			const accordion = dom.window.document.querySelector('[data-accordion]');
+
+			const button = accordion.querySelector('[data-accordion-button]');
+			const contentArea = accordion.querySelector('[data-accordion-content-area]');
+
+			// set the first one to be active
+			button.setAttribute('aria-expanded', true);
+			contentArea.setAttribute('aria-hidden', false);
+
+			button.addEventListener('click', (event) => {
+				if(contentArea.getAttribute('aria-hidden') === 'true') {
+					truthy = true;
+				}
+			});
+
+			button.dispatchEvent(Click);
+
+			truthy.should.equal(true);
 		});
 	});
 };
